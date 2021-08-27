6d0aa001547e2dd2234a8a18a60b0f91899c9b17
Utpal Pati <utpalpati8@gmail.com>
2021-08-27 16:51:12 +0000

    refactorr: section6 added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Section2 from './Section2';
 import Section3 from './Section3';
 import Section4 from "./Section4";
 import Section5 from './Section5';
+import Section6 from './Section6';
 function App() {
 
 
@@ -15,6 +16,7 @@ function App() {
       <Section3/>
       <Section4/>
       <Section5/>
+      <Section6/>
     </div>
   );
 }
---
+++ frontend/src/Section6.jsx
@@ -0,0 +1,28 @@
+import {FaFacebook, FaInstagram, FaTwitter, FaGithub, FaBasketballBall} from "react-icons/fa"
+export default function Section6(){
+
+    return(
+        <div className="flex flex-col px-2 sm:px-14 py-4 sm:py-6 items-center">
+            <div className="flex flex-wrap text-gray-400">
+                <a className=" m-2 sm:mx-3">About</a>
+                <a className=" m-2 sm:mx-3">Blog</a>
+                <a className=" m-2 sm:mx-3">Team</a>
+                <a className=" m-2 sm:mx-3">Pricing</a>
+                <a className=" m-2 sm:mx-3">Contact</a>
+                <a className=" m-2 sm:mx-3">Terms</a>
+
+            </div>
+            <div className="flex text-lg text-gray-400">
+                <FaFacebook className=" m-3 sm:m-4"/>
+                <FaInstagram className=" m-3 sm:m-4"/>
+                <FaTwitter className=" m-3 sm:m-4"/>
+                <FaGithub className=" m-3 sm:m-4"/>
+                <FaBasketballBall className=" m-3 sm:m-4"/>
+
+            </div>
+            <div className="text-gray-400 text-xs pt-2">
+            © 2021 SomeCompany, Inc. All rights reserved.
+            </div>
+        </div>
+    )
+}
