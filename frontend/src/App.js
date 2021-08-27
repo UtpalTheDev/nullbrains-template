0f18d1e0d7f9af5140effa0639cd899ab3b2c16e
Utpal Pati <utpalpati8@gmail.com>
2021-08-27 07:16:56 +0000

    refcator: section3 added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Section1 from './Section1';
 import Section2 from './Section2';
+import Section3 from './Section3';
 function App() {
 
 
@@ -9,7 +10,7 @@ function App() {
     <div className="main">
       <Section1/>
       <Section2/>
-      
+      <Section3/>
     </div>
   );
 }
